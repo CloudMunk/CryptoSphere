419db3bb1ca519014be496e406992af5334b98c1
CloudOne <munkeyten@protonmail.com>
2018-04-05 23:34:03 +0300

    0.004

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -75,7 +75,7 @@ class Landingpage extends Component {
                 <div className="section no-pad-bot">
                 <div className="container">
                     <div className="row center">
-                    <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                    <h5 className="header col s12 light">A modern new way of organizing everything Crypto</h5>
                     </div>
                 </div>
                 </div>
